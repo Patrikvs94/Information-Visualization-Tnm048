2623c4fde0598b9fa96df98e00817c131eeb8034
Patve575 <patrikvs94@gmail.com>
2019-03-05 11:25:37 +0100

    Attempt at getting a bar chart in our window
    Trying out D3.js to create a basic bar chart with dummy data.

--- js/chart.js
+++ js/chart.js
@@ -1,32 +1,38 @@
 
+var years = [2012, 2013, 2014, 2015, 2016, 2017];
+
 //Function to initialize our chart
 
 function initializeChart() {
-	var ctx = document.getElementById("chartid");
-	const margin = 60;
-	const width = 1000 - 2*margin;
-	const height = 600 - 2*margin;
 	
-	const svg = d3.select('svg');
-	
-	const chart = svg.append('g')
-		.attr('transform', 'translate(${margin}, ${margin})');
-		
-	const yScale = d3.scaleLinear()
-		.range([height, 0])
-		.domain([0, 100]);
-		
-	chart.append('g')
-		.call(d3.axisLeft(yScale));
-		
-	const xScale = d3.scaleBand()
-		.range([0, width])
-		.domain(sample.map((s) => s.language))
-		.padding(0.2)
+var svg = d3.select('svg'),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 1110 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-	chart.append('g')
-		.attr('transform', 'translate(0, ${height})')
-		.call(d3.axisBottom(xScale));
+var x = d3.scaleLinear().rangeRound([0, width]),
+    y = d3.scaleLinear().rangeRound([height, 0]);
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  x.domain(years);
+  y.domain([0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000]);
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10, "%"))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Frequency");
 }
 
 initializeChart();
