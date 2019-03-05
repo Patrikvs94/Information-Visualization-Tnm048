d9f79e1fe8e920afd4a0e6fdb97104a76c61dce4
Beeckaa <rebeckalindskog@gmail.com>
2019-03-05 11:48:19 +0100

    Bar chart

--- js/chart.js
+++ js/chart.js
@@ -1,38 +1,216 @@
-
-var years = [2012, 2013, 2014, 2015, 2016, 2017];
-
 //Function to initialize our chart
 
 function initializeChart() {
-	
-var svg = d3.select('svg'),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 1110 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var data = [
+  {
+      "categorie": "2013", 
+      "values": [
+          {
+              "value": 1, 
+              "rate": "Uddevalla"
+          }, 
+          {
+              "value": 4, 
+              "rate": "Lilla Edet"
+          }, 
+          {
+              "value": 12, 
+              "rate": "Kungälv"
+          }
+      ]
+  }, 
+  {
+      "categorie": "2014", 
+      "values": [
+          {
+              "value": 1, 
+              "rate": "Uddevalla"
+          }, 
+          {
+              "value": 21, 
+              "rate": "Lilla Edet"
+          }, 
+          {
+              "value": 13, 
+              "rate": "Kungälv"
+          }
+      ]
+  }, 
+  {
+      "categorie": "2015", 
+      "values": [
+          {
+              "value": 3, 
+              "rate": "Uddevalla"
+          }, 
+          {
+              "value": 22, 
+              "rate": "Lilla Edet"
+          }, 
+          {
+              "value": 6, 
+              "rate": "Kungälv"
+          }
+      ]
+  }, 
+  {
+      "categorie": "2016", 
+      "values": [
+          {
+              "value": 12, 
+              "rate": "Uddevalla"
+          }, 
+          {
+              "value": 7, 
+              "rate": "Lilla Edet"
+          }, 
+          {
+              "value": 18, 
+              "rate": "Kungälv"
+          }
+      ]
+  }, 
+  {
+      "categorie": "2017", 
+      "values": [
+          {
+              "value": 6, 
+              "rate": "Uddevalla"
+          }, 
+          {
+              "value": 15, 
+              "rate": "Lilla Edet"
+          }, 
+          {
+              "value": 9, 
+              "rate": "Kungälv"
+          }
+      ]
+  }, 
+  {
+      "categorie": "2018", 
+      "values": [
+          {
+              "value": 6, 
+              "rate": "Uddevalla"
+          }, 
+          {
+              "value": 6, 
+              "rate": "Lilla Edet"
+          }, 
+          {
+              "value": 7, 
+              "rate": "Kungälv"
+          }
+      ]
+  }
+];
+var years = [2012, 2013, 2014, 2015, 2016, 2017];
+  
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-var x = d3.scaleLinear().rangeRound([0, width]),
-    y = d3.scaleLinear().rangeRound([height, 0]);
+    var x0 = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var x1 = d3.scale.ordinal();
 
-  x.domain(years);
-  y.domain([0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000]);
+    var y = d3.scale.linear()
+        .range([height, 0]);
 
-  g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    var xAxis = d3.svg.axis()
+        .scale(x0)
+        .tickSize(0)
+        .orient("bottom");
 
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "%"))
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var color = d3.scale.ordinal()
+        .range(["#d5d5d5","#92c5de","#0571b0"]);
+
+    var svg = d3.select('body').append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var categoriesNames = data.map(function(d) { return d.categorie; });
+    var rateNames = data[0].values.map(function(d) { return d.rate; });
+
+    x0.domain(categoriesNames);
+    x1.domain(rateNames).rangeRoundBands([0, x0.rangeBand()]);
+    y.domain([0, d3.max(data, function(categorie) { return d3.max(categorie.values, function(d) { return d.value; }); })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .style('opacity','0')
+        .call(yAxis)
     .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Frequency");
-}
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .style('font-weight','bold')
+        .text("People/mi2");
 
-initializeChart();
+    svg.select('.y').transition().duration(500).delay(1300).style('opacity','1');
+
+    var slice = svg.selectAll(".slice")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "g")
+        .attr("transform",function(d) { return "translate(" + x0(d.categorie) + ",0)"; });
+
+    slice.selectAll("rect")
+        .data(function(d) { return d.values; })
+    .enter().append("rect")
+        .attr("width", x1.rangeBand())
+        .attr("x", function(d) { return x1(d.rate); })
+        .style("fill", function(d) { return color(d.rate) })
+        .attr("y", function(d) { return y(0); })
+        .attr("height", function(d) { return height - y(0); })
+        .on("mouseover", function(d) {
+            d3.select(this).style("fill", d3.rgb(color(d.rate)).darker(2));
+        })
+        .on("mouseout", function(d) {
+            d3.select(this).style("fill", color(d.rate));
+        });
+
+    slice.selectAll("rect")
+        .transition()
+        .delay(function (d) {return Math.random()*1000;})
+        .duration(1000)
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); });
+
+    //Legend
+    var legend = svg.selectAll(".legend")
+        .data(data[0].values.map(function(d) { return d.rate; }).reverse())
+    .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d,i) { return "translate(0," + i * 20 + ")"; })
+        .style("opacity","0");
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function(d) { return color(d); });
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) {return d; });
+
+    legend.transition().duration(500).delay(function(d,i){ return 1300 + 100 * i; }).style("opacity","1");
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
     initializeMap();
+    initializeChart();
     getData().then(addTopoToMap);
 });
