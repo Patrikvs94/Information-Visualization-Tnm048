6421b6973d734bd1811566cb6931b83c38d10bdb
Ola Steen <ola_stenis@hotmail.com>
2019-03-08 09:51:31 +0100

    fixed bug where municipalities were deselected on search

--- js/map.js
+++ js/map.js
@@ -176,9 +176,11 @@ function restyleLayer(selectedYear) {
 function searchLayer(municipality) {
 	geojson.eachLayer(function(featuredInstancelayer) {
 		if(!featuredInstancelayer.hasOwnProperty("KNNAMN")) {
+            
 			if(featuredInstancelayer != null && featuredInstancelayer.feature.properties.KNNAMN.toLowerCase() == municipality.toLowerCase()) {
                 var theLayer = featuredInstancelayer;
-                markFeature({target : theLayer});
+                if(theLayer.marked == null || theLayer.marked == false)
+                    markFeature({target : theLayer});
 			}
 		}
 	});
