90a50bfa4c5c99e6c803a3529072c850ff8ea370
Beeckaa <rebeckalindskog@gmail.com>
2019-03-08 09:45:33 +0100

    Search bar

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
     initializeMap();
     //initializeChart();
     getData().then(addTopoToMap);
+    document.getElementById("searchForm").onsubmit = function() {searchLayer(document.getElementById("searchText").value)};
 });
